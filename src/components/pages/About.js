46ac898f9926041af83361939e277bf423edd6df
zmathew <zmathew@ncsu.edu>
2019-01-19 07:20:56 -0500

    Add About component

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the todo app v1.0. It is part of a react crash course.</p>
+    </div>
+  )
+}
