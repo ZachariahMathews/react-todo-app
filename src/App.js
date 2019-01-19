6f870b212a85071dfb32cc33af7da3b369b03615
zmathew <zmathew@ncsu.edu>
2019-01-19 07:21:35 -0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/layout/Header'
 import Todos from './components/Todos'
 import AddTodo from './components/AddTodo'
+import About from './components/pages/About'
 import uuid from 'uuid'
 
 import './App.css';
@@ -51,11 +53,19 @@ state={
 
   render() {
     return (
-      <div className="App">
-        <Header />
-        <AddTodo addTodo={this.addTodo} />
-        <Todos todos={this.state.todos} markComplete={this.markComplete} deleteTodo={this.deleteTodo} />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/" render={props =>(
+            <React.Fragment>
+              <AddTodo addTodo={this.addTodo} />
+              <Todos todos={this.state.todos} markComplete={this.markComplete} deleteTodo={this.deleteTodo} />
+            </React.Fragment>
+          )} />
+          <Route path="/about" component={About} />
+          
+        </div>
+      </Router>
     );
   }
 }
